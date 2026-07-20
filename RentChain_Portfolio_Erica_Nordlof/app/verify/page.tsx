1560d5cd7cef1bbf2fb77c65ac78db85663afef1
Erica Nordlöf <ericcka86_@outlook.com>
2026-07-20 17:59:44 +0200

    Delete RentChain_Portfolio_Erica_Nordlof directory

--- RentChain_Portfolio_Erica_Nordlof/app/verify/page.tsx
+++
@@ -1,9 +0,0 @@
-import { VerifyTool } from "@/components/VerifyTool";
-
-export default function VerifyPage() {
-  return (
-    <div className="page-shell narrow-shell">
-      <VerifyTool />
-    </div>
-  );
-}
